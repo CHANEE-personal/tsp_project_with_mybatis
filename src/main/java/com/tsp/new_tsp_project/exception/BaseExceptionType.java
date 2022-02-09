3d7b26afd46521df353fce20d9c20789c92a0528
ChoChanHui <chanhee.cho@ppbstudios.com>
2022-02-09 22:57:27 +0900

    주석 커밋

--- src/main/java/com/tsp/new_tsp_project/exception/BaseExceptionType.java
+++ src/main/java/com/tsp/new_tsp_project/exception/BaseExceptionType.java
@@ -2,7 +2,10 @@ package com.tsp.new_tsp_project.exception;
 
 public interface BaseExceptionType {
 
+	// errorCode
 	String getErrorCode();
+	// HttpStatus
 	int getHttpStatus();
+	// errorMessage
 	String getErrorMessage();
 }
