e7b1d6f04591972d57e223a9bbd1a2fef6ab0727
Cho Chan Hee <cksgml4424@gmail.com>
2022-02-20 21:10:53 +0900

    test commit

--- src/test/java/com/tsp/new_tsp_project/api/admin/model/controller/AdminModelJpaApiTest.java
+++ src/test/java/com/tsp/new_tsp_project/api/admin/model/controller/AdminModelJpaApiTest.java
@@ -1,7 +1,11 @@
 package com.tsp.new_tsp_project.api.admin.model.controller;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class AdminModelJpaApiTest {
 
 }
