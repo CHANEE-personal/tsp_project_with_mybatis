8905a1e6167d6b86d09c6e2e7b2e7dfd9f34ba2a
CHO CHAN HEE <cksgml4424@gmail.com>
2022-06-29 15:00:50 +0900

    관리자 회원 관련 junit test 추가

---
+++ src/test/java/com/tsp/new_tsp_project/api/admin/user/controller/AdminUserApiTest.java
@@ -0,0 +1,96 @@
+package com.tsp.new_tsp_project.api.admin.user.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tsp.new_tsp_project.api.jwt.AuthenticationRequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import javax.transaction.Transactional;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@Transactional
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application.properties")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class AdminUserApiTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    @BeforeEach
+    void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(wac)
+                .addFilter(new CharacterEncodingFilter("UTF-8", true))
+                .apply(springSecurity())
+                .alwaysDo(print())
+                .build();
+    }
+
+    @Test
+    @DisplayName("Admin 관리자 조회 테스트")
+    void 관리자조회Api테스트() throws Exception {
+        MultiValueMap<String, String> userMap = new LinkedMultiValueMap<>();
+        userMap.add("jpaStartPage", "1");
+        userMap.add("size", "3");
+        mockMvc.perform(get("/api/auth/users").params(userMap));
+    }
+
+    @Test
+    @DisplayName("Admin 로그인 처리 테스트")
+    void 관리자로그인Api테스트() throws Exception {
+        AuthenticationRequest authenticationRequest = new AuthenticationRequest();
+        authenticationRequest.setUserId("admin01");
+        authenticationRequest.setPassword("pass1234");
+
+        mockMvc.perform(post("/api/auth/admin-login")
+                        .contentType(APPLICATION_JSON_VALUE)
+                        .content(objectMapper.writeValueAsString(authenticationRequest)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.loginYn").value("Y"))
+                .andExpect(jsonPath("$.userId").value("admin01"))
+                .andExpect(jsonPath("$.token").isNotEmpty());
+    }
+
+    @Test
+    @DisplayName("Admin JWT 토큰 발급 테스트")
+    void JWT토큰발급Api테스트() throws Exception {
+        AuthenticationRequest authenticationRequest = new AuthenticationRequest();
+        authenticationRequest.setUserId("admin01");
+        authenticationRequest.setPassword("pass1234");
+
+        mockMvc.perform(post("/api/auth/authenticate")
+                .contentType(APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(authenticationRequest)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.jwt").isNotEmpty())
+                .andExpect(jsonPath("$.token").isNotEmpty());
+    }
+}
