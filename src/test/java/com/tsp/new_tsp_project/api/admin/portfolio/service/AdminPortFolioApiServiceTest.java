dde67f2184dc2abfc76c8bd266a9c610917371d4
CHO CHAN HEE <cksgml4424@gmail.com>
2022-07-01 11:33:42 +0900

    portfolio service 관련 junit test commit

--- src/test/java/com/tsp/new_tsp_project/api/admin/portfolio/service/AdminPortFolioApiServiceTest.java
+++ src/test/java/com/tsp/new_tsp_project/api/admin/portfolio/service/AdminPortFolioApiServiceTest.java
@@ -1,6 +1,6 @@
-package com.tsp.new_tsp_project.api.admin.production.service;
+package com.tsp.new_tsp_project.api.admin.portfolio.service;
 
-import com.tsp.new_tsp_project.api.admin.production.domain.dto.AdminProductionDTO;
+import com.tsp.new_tsp_project.api.admin.portfolio.domain.dto.AdminPortFolioDTO;
 import com.tsp.new_tsp_project.api.common.domain.dto.CommonImageDTO;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,13 +13,12 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.transaction.Transactional;
-
 import java.io.FileInputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static com.tsp.new_tsp_project.api.admin.production.domain.dto.AdminProductionDTO.*;
+import static com.tsp.new_tsp_project.api.admin.portfolio.domain.dto.AdminPortFolioDTO.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -27,33 +26,34 @@ import static org.assertj.core.api.Assertions.assertThat;
 @AutoConfigureMockMvc
 @TestPropertySource(locations = "classpath:application-local.properties")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-class AdminProductionApiServiceTest {
+class AdminPortFolioApiServiceTest {
 
     @Autowired
-    private AdminProductionApiService adminProductionApiService;
+    private AdminPortFolioApiService adminPortFolioApiService;
 
     @Test
-    @DisplayName("프로덕션 리스트 조회 테스트")
-    void 프로덕션리스트조회테스트() throws Exception {
-        Map<String, Object> productionMap = new HashMap<>();
-        productionMap.put("startPage", 1);
-        productionMap.put("size", 3);
-        List<AdminProductionDTO> productionList = adminProductionApiService.getProductionList(productionMap);
-        assertThat(productionList).isNotEmpty();
+    @DisplayName("포트폴리오 리스트 조회 테스트")
+    void 포트폴리오리스트조회테스트() throws Exception {
+        Map<String, Object> portfolioMap = new HashMap<>();
+        portfolioMap.put("startPate", 1);
+        portfolioMap.put("size", 3);
+        List<AdminPortFolioDTO> portfoliList = adminPortFolioApiService.getPortFolioList(portfolioMap);
+        assertThat(portfoliList).isNotEmpty();
     }
 
     @Test
-    @DisplayName("프로덕션 상세 조회 테스트")
-    void 프로덕션상세조회테스트() throws Exception {
-        AdminProductionDTO adminProductionDTO = builder().idx(117).build();
-        assertThat(adminProductionApiService.getProductionInfo(adminProductionDTO).get("productionInfo")).isNotNull();
+    @DisplayName("포트폴리오 상세 조회 테스트")
+    void 포트폴리오상세조회테스트() throws Exception {
+        AdminPortFolioDTO adminPortFolioDTO = builder().idx(1).build();
+        assertThat(adminPortFolioApiService.getPortFolioInfo(adminPortFolioDTO)).isNotEmpty();
     }
 
     @Test
-    @DisplayName("프로덕션 등록 테스트")
-    void 관리자프로덕션등록테스트() throws Exception {
-        AdminProductionDTO adminProductionDTO = builder()
-                .title("테스트").description("테스트").visible("Y").build();
+    @DisplayName("포트폴리오 등록 테스트")
+    void 포트폴리오등록테스트() throws Exception {
+        AdminPortFolioDTO adminPortFolioDTO = builder()
+                .categoryCd(1).title("테스트").description("테스트").videoUrl("https://youtube.com")
+                .visible("Y").hashTag("#test").build();
 
         CommonImageDTO commonImageDTO = CommonImageDTO.builder()
                 .idx(1)
@@ -72,15 +72,15 @@ class AdminProductionApiServiceTest {
                         "image/png" , new FileInputStream("src/main/resources/static/images/0522045010772.png"))
         );
 
-        Integer count = adminProductionApiService.insertProduction(adminProductionDTO, commonImageDTO, imageFiles);
+        Integer count = adminPortFolioApiService.insertPortFolio(adminPortFolioDTO, commonImageDTO, imageFiles);
         assertThat(count).isPositive();
     }
 
     @Test
-    @DisplayName("프로덕션 삭제 테스트")
-    void 프로덕션삭제테스트() throws Exception {
-        AdminProductionDTO adminProductionDTO = builder().idx(117).build();
-        Integer count = adminProductionApiService.deleteProduction(adminProductionDTO);
+    @DisplayName("포트폴리오 삭제 테스트")
+    void 포트폴리오삭제테스트() throws Exception {
+        AdminPortFolioDTO adminPortFolioDTO = builder().idx(1).build();
+        Integer count = adminPortFolioApiService.deletePortFolio(adminPortFolioDTO);
         assertThat(count).isPositive();
     }
 }
